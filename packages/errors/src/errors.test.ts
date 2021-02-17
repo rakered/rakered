742a4037ab064b6518a369aa88c9ac04cdd84a34
Stephan Meijer <stephan.meijer@gmail.com>
2021-02-17 10:55:22 +0100

    feat(errors): support data on UserInputError

--- packages/errors/src/errors.test.ts
+++ packages/errors/src/errors.test.ts
@@ -1,4 +1,5 @@
 import * as errors from './errors';
+import { UserInputError } from './errors';
 
 test('errors return name and code', () => {
   for (const CustomError of Object.values(errors)) {
@@ -7,4 +8,16 @@ test('errors return name and code', () => {
     expect(error.message).toEqual('some error');
     expect(typeof error.code).toEqual('number');
   }
+});
+
+test('UserInputError can hold data', () => {
+  const error = new UserInputError('some error', {
+    input: 'name',
+    message: 'required',
+  });
+
+  expect(error.data).toEqual({
+    input: 'name',
+    message: 'required',
+  });
 });
--- packages/errors/src/errors.ts
+++ packages/errors/src/errors.ts
@@ -11,6 +11,15 @@ export class ForbiddenError extends Error {
 export class UserInputError extends Error {
   name = 'UserInputError';
   code = 422;
+  data;
+
+  constructor(
+    message: string,
+    data?: Record<string, any> | Record<string, any>[] | string[],
+  ) {
+    super(message);
+    this.data = data;
+  }
 }
 
 export class NotFoundError extends Error {
